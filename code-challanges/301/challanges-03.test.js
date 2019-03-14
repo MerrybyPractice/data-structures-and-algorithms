afa990cd3078889a059d47f3560cd0a1f3340032
Merry Cimakasky <mkcimakasky@gmail.com>
2019-03-13 22:44:25 -0700

    pushing to test 7

--- code-challanges/301/challanges-03.test.js
+++ code-challanges/301/challanges-03.test.js
@@ -84,7 +84,7 @@ For example, [1, 14, 0.2, -281, 54782] is only correctly sorted in that order.
 
 const sortNumbersByLength = (arr) => {
   return arr.sort( (a,b) => {
-    return (a.toString > b.toString);
+    return (a.toString().length > b.toString().length);
   })
 };
 
@@ -107,7 +107,9 @@ const people = [
 ];
 
 const sortPeople = (arr) => {
-  // Solution code here...
+  return arr.sort((a,b) => {
+    return (a.lastName < b.lastName)
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
