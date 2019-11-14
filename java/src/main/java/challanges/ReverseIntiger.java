4cf97a38d46c1ab24b22c77e7f0fdb30f254dd88
Merry Cimakasky <mkcimakasky@gmail.com>
2019-11-13 21:20:51 -0800

    a string builder method?

--- java/src/main/java/challanges/ReverseIntiger.java
+++ java/src/main/java/challanges/ReverseIntiger.java
@@ -1,61 +1,41 @@
 package challanges;
 
-import static java.util.Arrays.copyOfRange;
+import static java.lang.Integer.parseInt;
+import static java.lang.Integer.toUnsignedLong;
+
 
 
 public class ReverseIntiger {
 
         public int reverse (int x) {
 
-            int returnValue;
+            boolean isNegative = false;
 
-            Integer integerX = (Integer) x;
-
-            String stringX = integerX.toString();
-
-            char[] charX = stringX.toCharArray();
-
-            char negative = 0;
-
-            int length = charX.length;
-
-            char[] charXTwo = charX;
-
-            if (charX[0] == '-'){
-
-                negative = charX[0];
-
-                charXTwo = copyOfRange(charX, 1, length);
+            if (x < 0){
+                isNegative = true;
+                x *= -1;
             }
 
-            char[] reversed;
-            reversed = new char[charXTwo.length];
+            String stringyX = String.valueOf(x);
 
-            for (int i = charXTwo.length; i >= 0; i--){
+            stringyX = reverseString(stringyX);
 
-                if (negative == '-'){
+            Integer returnValue = parseInt(stringyX);
 
-                    reversed[0] = '-';
-                    reversed[i+1] = charXTwo[i];
-                } else {
-
-                    reversed[i] = charXTwo[i];
-                }
+            if(isNegative == true){
+                returnValue *= -1;
             }
 
-            String reversedString = reversed.toString();
-
-
-            try{
-                returnValue = Integer.parseInt(reversedString, 10);
-            }
-            catch (NumberFormatException e){
-
-                returnValue = 0;
-
-            }
 
             return returnValue;
+        }
+
+        public String reverseString(String stringyX){
+
+            StringBuilder build = new StringBuilder(stringyX);
+
+            return build.reverse().toString();
+
         }
     }
 
