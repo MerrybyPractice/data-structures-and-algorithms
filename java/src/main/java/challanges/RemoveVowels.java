678fee95ca64efcaec0644ee7a289faa00d42489
Merry Cimakasky <mkcimakasky@gmail.com>
2019-11-14 16:27:42 -0800

    refactored with a string builder, accpected by leet

--- java/src/main/java/challanges/RemoveVowels.java
+++ java/src/main/java/challanges/RemoveVowels.java
@@ -7,47 +7,21 @@ public class RemoveVowels {
 
     public String removeVowels(String S) {
 
-        ArrayList<Character> charArrayS = new ArrayList<Character>();
+        char[] charArrayS = S.toCharArray();
 
-        for (char c : S.toCharArray()) {
-            charArrayS.add(c);
+        StringBuilder rebuildS = new StringBuilder(S.length());
+
+       for (int i = 0; i < S.length(); i++) {
+
+            if (charArrayS[i] != 'a' && charArrayS[i] != 'e' && charArrayS[i] != 'i' && charArrayS[i] != 'o' && charArrayS[i] != 'u') {
+
+                rebuildS.append(charArrayS[i]);
+
+            }
+
         }
 
-        List<Character> vowels = new ArrayList<Character>();
-        vowels.add('a');
-        vowels.add('e');
-        vowels.add('i');
-        vowels.add('o');
-        vowels.add('u');
-
-        while (charArrayS.containsAll(vowels)) {
-
-            if (charArrayS.contains('a')) {
-
-                charArrayS.remove('a');
-            }
-
-            if (charArrayS.contains('e')) {
-
-                charArrayS.remove('e');
-            }
-
-            if (charArrayS.contains('i')) {
-
-                charArrayS.remove('i');
-
-            }
-
-            if (charArrayS.contains('o')) {
-
-                charArrayS.remove('o');
-
-            }
-        }
-
-        String returnValue = charArrayS.toString();
-
-        return returnValue;
+        return rebuildS.toString();
 
     }
 
