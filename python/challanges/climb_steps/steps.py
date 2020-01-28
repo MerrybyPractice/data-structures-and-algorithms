8efffb5af444be588ff41a083df32cb44536d2da
Merry Cimakasky <mkcimakasky@gmail.com>
2020-01-27 19:36:09 -0800

    stairs done

---
+++ python/challanges/climb_steps/steps.py
@@ -0,0 +1,10 @@
+# loop over stairs with for loop 
+# each even number adds two ways of climbing the stairs
+
+def climb(n): 
+    if n <= 0 
+        return n 
+    else: 
+        return (climb(n-1) + climb(n-2))
+        
+
